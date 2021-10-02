75d26f74190e0076bb9d02e1189381fe64ec4847
ByreddyKundana <byreddykundana@gmail.com>
2021-10-02 17:29:44 +0530

    dark_mode

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 var heading=document.getElementById('heading')
-heading.innerHTML="hi from js"
+var body=document.querySelector('body')
 console.log(heading)
-console.log(typeof heading)
-var para=document.getElementsByClassName('para')
-console.log(para)//html collection
-console.log(para.length)
-console.log(para[0])
-console.log(para[1])
 
-var tags=document.getElementsByTagName('a')
-console.log(tags)
-
-var query=document.querySelector('.para')
-console.log(query)
+heading.innerHTML="i was added by JS"
+heading.style="color:red;font-size:2rem"
+console.log(heading.classList)
+heading.classList.add('five')
+console.log(heading.classList)
+heading.classList.remove('one')
+console.log(heading.classList)
+body.classList.add('dark')
