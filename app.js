d661d96d8b1b97bf42ae307776410128d6d6f54b
ByreddyKundana <byreddykundana@gmail.com>
2021-10-02 22:47:03 +0530

    dark_mode

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 var button=document.querySelector('button')
-var input=document.querySelector('input')
-var list=document.querySelector('ul')
+var body=document.querySelector('body')
 
-//const callbackfunc=(event)=>{
- //   console.log(event.target)
- //   console.log(input.value)
-//}
-
-//button.addEventListener('click',callbackfunc)
 
 const callbackfunc=(event)=>{
-    const inputvalue=input.value
-    const element=document.createElement('li')
-    const textnode=document.createTextNode(inputvalue)
-    element.appendChild(textnode)
-    list.appendChild(element)
-}
+    body.classList.toggle('dark')
+}    
 button.addEventListener('click',callbackfunc)
