78dbe525fb9a47c52f82a5e6e8e9c8cda2af231c
ByreddyKundana <byreddykundana@gmail.com>
2021-10-02 11:44:15 +0530

    primitive_datatypes

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log('hello world')
+console.warn('hello warning')
+console.error('hello error')
+//primitive data types
+//string
+
+console.log("hello world")
+console.log(typeof "hello world")
+console.log(typeof'd')
+console.log(typeof"d")
+
+//numbers
+
+console.log(typeof 1)
+console.log(typeof -1.2)
+console.log(typeof 12344858575478459384284375857458324)
+console.log(typeof '1')//string
+
+//boolean
+
+console.log(typeof true)
+console.log(typeof false)
+
