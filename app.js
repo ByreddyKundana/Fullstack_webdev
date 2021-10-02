3ff6a4b94aff374c4e6bb56d2937770e25e8ca23
ByreddyKundana <byreddykundana@gmail.com>
2021-10-02 12:16:39 +0530

    array_var_const

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-//var age=19
-//console.log(age)
-var age
+//non primitive data types
+//array is an object
+
+var arr=[1,2,3,4]
+console.log(arr) 
+console.log(arr.length)
+arr.push(5)
+console.log(arr)
+
+//array in js is not generic
+
+var arr=[1,2,3,4,"age"]
+console.log(arr)
+console.log(typeof arr)
+console.log(arr.length)
+
+var age=19
+age=43
 console.log(age)
-console.log(typeof age)
-var age=null
-console.log(age)
-console.log(typeof age)
-console.log(Name)
+const set=19
+set=43
+console.log(set)
