0ec3f661bbc18d31531333caca72b3c6cfab3ed8
ByreddyKundana <byreddykundana@gmail.com>
2021-10-03 16:24:23 +0530

    calculator

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-var button=document.querySelector('button')
-var body=document.querySelector('body')
+const a=document.querySelectorAll('input')[0]
+const b=document.querySelectorAll('input')[1]
+const add=document.querySelector('#add')
+const multiply=document.querySelector('#multiply')
+const subract=document.querySelector('#subract')
+const resultbox=document.querySelector('.result')
+ 
+//const sum=()=>{
+  //  const result=parseInt(a.value)+parseInt(b.value)
+    //resultbox.innerHTML=result
+//}
 
+//const product=()=>{
+ //   const result=parseInt(a.value)+parseInt(b.value)
+   // resultbox.innerHTML=result
+//}
 
-const callbackfunc=(event)=>{
-    body.classList.toggle('dark')
-}    
-button.addEventListener('click',callbackfunc)
+const calculate=(event,operation)=>{
+  //  console.log(event.target)
+   //if(operation=='add') {
+     //  console.log('adding')
+   //}else if (operation=='multiply'){
+     //  console.log('multiply')
+   //}
+   switch(operation){
+       case "add":
+           resultbox.innerHTML=parseInt(a.value)+parseInt(b.value)
+           break
+      case "multiply":
+           resultbox.innerHTML=parseInt(a.value)*parseInt(b.value)
+           break
+      case "subract":
+           resultbox.innerHTML=parseInt(a.value)-parseInt(b.value)
+           break    
+      default:
+          resultbox.innerHTML="Not a valid operation"
+   }
+}
+multiply.addEventListener('click',(event)=>{
+    calculate(event,'multiply')
+})
+add.addEventListener('click',(event)=>{
+    calculate(event,'add')
+})
+subract.addEventListener('click',(event)=>{
+    calculate(event,'subract')
+})
