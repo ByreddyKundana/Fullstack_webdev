b16c757310b2912e39406ba3bcfe5dabb240f01c
ByreddyKundana <byreddykundana@gmail.com>
2021-10-03 19:36:47 +0530

    array_splice

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 var arr=[1,2,3,4,5,6,7,8]
 
-//arr.shift()
-//var new_arr =arr.slice(2,5)
+console.log(arr.splice(3,3))
 
-//console.log(new_arr)
-var new_arr=arr.filter((element,index)=>{
-  if (element%2===0) return true
-})
-
-//for(var i=0;i<arr.length;i++){
-//  if (arr[i]%2===0) new_arr.push(arr[i])
-//}
-console.log(new_arr)
+console.log(arr) 
