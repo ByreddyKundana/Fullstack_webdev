ba0503c205cb949e414d15f3a67fe6c77174dc0a
ByreddyKundana <byreddykundana@gmail.com>
2021-10-05 15:43:10 +0530

    setTimeout

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
-//console.log(this.alert('HI'))
- //let manufacturer="ferrari"
-let car={
-    name: "c calss",
-    manufacturer:"mercedes",
-    print: ()=>{
-        // console.log("a")
-        console.log(this)
-        //let manufacturer="BMW"
-        //console.log(manufacturer) 
-        //console.log(car.manufacturer,car.name)
-        //console.log(this.name,this.manufacturer)
-        console.log(`${this.name} was created by ${this.manufacturer}`)
-    }
-}
-// car.print('ferrari')
-// console.log(car.name)
-// console.log(car.manufacturer)
-// console.log(car)
-// console.log(typeof car)
-//car.print()
-
-
-// console.log(Object.keys(car))
-// console.log(Object.values(car))
-console.log(car.hasOwnProperty('name'))
+setTimeout(()=>{
+    console.log('priya')
+},0)
+console.log('hi')
+console.log('hey')
